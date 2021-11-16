3ce5d7dc0f8467d5799b6a53443661b518284f3e
david-chavez-grant <me@davidgrant.dev>
2021-11-16 15:55:23 -0500

    Add Form Data

---
+++ Data/SubmissionFormData.cs
@@ -0,0 +1,11 @@
+namespace Bylines.LandingPage.Data
+{
+	public class SubmissionFormData
+	{
+		public string Name        { get; set; }
+		public string Email       { get; set; }
+		public string PhoneNumber { get; set; }
+		public bool   IsGroup     { get; set; }
+	}
+
+}
